4015a17d44df30054e035b7496ef27d17532b810
Alexander Kravchenko <alex.kravchenko@protonmail.com>
2021-11-30 21:24:42 +0200

    progress update

--- app/src/main/java/com/example/a7min_worki/MainActivity.kt
+++ app/src/main/java/com/example/a7min_worki/MainActivity.kt
@@ -4,18 +4,28 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.FrameLayout
 import android.widget.Toast
+import com.example.a7min_worki.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private var binding: ActivityMainBinding? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
 
-        val flStartButton: FrameLayout = findViewById(R.id.flStart)
-        flStartButton.setOnClickListener{
+        binding?.flStart?.setOnClickListener{
             Toast.makeText(this@MainActivity,
                 "Here we'll start the exercise",
                 Toast.LENGTH_SHORT
             ).show()
         }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+
+        binding = null
     }
 }
