bb11787e49ccfe3a10b927654a34a907c20756ca
Alexander Kravchenko <alex.kravchenko@protonmail.com>
2021-12-08 20:33:29 +0200

    progress update

--- app/src/main/java/com/example/a7min_worki/ExerciseActivity.kt
+++ app/src/main/java/com/example/a7min_worki/ExerciseActivity.kt
@@ -48,6 +48,11 @@ class ExerciseActivity : AppCompatActivity() {
         binding?.flExerciseView?.visibility = View.INVISIBLE
         binding?.ivImage?.visibility = View.INVISIBLE
 
+        binding?.tvUpcomingExerciseName?.text = exerciseList!![currentExercisePosition+1].getName()
+
+        binding?.tvUpcomingLabel?.visibility = View.VISIBLE
+        binding?.tvUpcomingExerciseName?.visibility = View.VISIBLE
+
         if(restTimer != null){
             restTimer?.cancel()
             restProgress = 0
@@ -62,6 +67,9 @@ class ExerciseActivity : AppCompatActivity() {
        binding?.tvExerciseName?.visibility = View.VISIBLE
        binding?.flExerciseView?.visibility = View.VISIBLE
        binding?.ivImage?.visibility = View.VISIBLE
+
+       binding?.tvUpcomingLabel?.visibility = View.INVISIBLE
+       binding?.tvUpcomingExerciseName?.visibility = View.INVISIBLE
 
         if(exerciseTimer != null){
             exerciseTimer?.cancel()
