b3b601f9385c709023a48f0eacdc92e1ebdff10a
Alexander Kravchenko <alex.kravchenko@protonmail.com>
2021-12-18 22:04:31 +0200

    progress update

--- app/src/main/java/com/example/a7min_worki/ExerciseActivity.kt
+++ app/src/main/java/com/example/a7min_worki/ExerciseActivity.kt
@@ -23,8 +23,8 @@ class ExerciseActivity : AppCompatActivity(), TextToSpeech.OnInitListener {
 
     private var restTimer: CountDownTimer? = null
     private var restProgress = 0
-    private var restTimerDuration: Long = 1
-    private var exerciseTimerDuration: Long = 3
+    private var restTimerDuration: Long = 2
+    private var exerciseTimerDuration: Long = 4
     private var exerciseTimer: CountDownTimer? = null
     private var exerciseProgress = 0
 
