7648525d01bb8c2ae02ece680c5330449a1bdffe
Alexander Kravchenko <alex.kravchenko@protonmail.com>
2021-12-23 21:26:11 +0200

    progress update

---
+++ app/src/main/java/com/example/a7min_worki/HistoryDao.kt
@@ -0,0 +1,10 @@
+package com.example.a7min_worki
+
+import androidx.room.Dao
+import androidx.room.Insert
+
+@Dao
+interface HistoryDao {
+    @Insert
+    suspend fun insert(historyEntity: HistoryEntity)
+}
