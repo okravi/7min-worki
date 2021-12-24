f6a7b4eaebdb112c3c0b150e11888353a9942869
Alexander Kravchenko <alex.kravchenko@protonmail.com>
2021-12-24 21:46:12 +0200

    progress update

--- app/src/main/java/com/example/a7min_worki/FinishActivity.kt
+++ app/src/main/java/com/example/a7min_worki/FinishActivity.kt
@@ -1,13 +1,18 @@
 package com.example.a7min_worki
 
 import android.os.Bundle
+import android.util.Log
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import com.example.a7min_worki.databinding.ActivityFinishBinding
+import kotlinx.coroutines.launch
+import java.text.SimpleDateFormat
+import java.util.*
 
 class FinishActivity : AppCompatActivity() {
 
@@ -33,5 +38,29 @@ class FinishActivity : AppCompatActivity() {
         binding?.btnFinish?.setOnClickListener{
             finish()
         }
+
+        val dao = (application as WorkoutApp).db.historyDao()
+        addDateToDataBase(dao)
+
     }
+
+    private fun addDateToDataBase(historyDao: HistoryDao){
+
+        val c = Calendar.getInstance()
+        val dateTime = c.time
+        Log.e("Date: ", "" + dateTime)
+
+        val sdf = SimpleDateFormat("dd MMM yyyy HH:mm:ss", Locale.getDefault())
+        val date = sdf.format(dateTime)
+        Log.e("Formatted Date: ", "" + date)
+
+        lifecycleScope.launch{
+            historyDao.insert(HistoryEntity(date))
+            Log.e(
+                "Date: ",
+                "Added..."
+            )
+        }
+    }
+
 }
