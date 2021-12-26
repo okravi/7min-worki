df09d39defe267e9505014de0e10de724455bfea
Alexander Kravchenko <alex.kravchenko@protonmail.com>
2021-12-26 15:37:03 +0200

    progress update

--- app/src/main/java/com/example/a7min_worki/HistoryActivity.kt
+++ app/src/main/java/com/example/a7min_worki/HistoryActivity.kt
@@ -3,7 +3,9 @@ package com.example.a7min_worki
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.a7min_worki.databinding.ActivityHistoryBinding
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
@@ -36,8 +38,25 @@ class HistoryActivity : AppCompatActivity() {
     private fun getAllCompletedDates(historyDao: HistoryDao){
         lifecycleScope.launch{
             historyDao.fetchAllDates().collect { allCompletedDatesList ->
-                for(i in allCompletedDatesList){
-                    Log.e("Date: ", ""+i)
+                if(allCompletedDatesList.isNotEmpty()){
+                    binding?.tvHistory?.visibility = View.VISIBLE
+                    binding?.rvHistory?.visibility = View.VISIBLE
+                    binding?.tvNoDataAvailable?.visibility = View.INVISIBLE
+
+                    binding?.rvHistory?.layoutManager = LinearLayoutManager(this@HistoryActivity)
+
+                    val dates = ArrayList<String>()
+                    for(date in allCompletedDatesList){
+                        dates.add(date.date)
+                    }
+
+                    val historyAdapter = HistoryAdapter(dates)
+                    binding?.rvHistory?.adapter = historyAdapter
+
+                }else{
+                    binding?.tvHistory?.visibility = View.GONE
+                    binding?.rvHistory?.visibility = View.GONE
+                    binding?.tvNoDataAvailable?.visibility = View.VISIBLE
                 }
             }
         }
